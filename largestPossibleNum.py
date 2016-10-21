7ec0afc6b18a30d2595ca2d8b831e8fff0eb9f88
Rakesh Kumar <gundetirakeshkumar@gmail.com>
2016-10-21 16:08:37 +0530

    Update largestPossibleNum.py

--- largestPossibleNum.py
+++ largestPossibleNum.py
@@ -9,14 +9,18 @@ else:
   
 l.sort(reverse = True)
 
+arr = ''.join(l)
 op = ''
 c = 0
 
-for i in a:
+for i in arr:
   op = op + i
   c = c + 1
   if c == 3:
     op = op + ','
     c = 0
     
+if op.endswith(","):
+	op = op[:len(op)-1]
+	
 print(op)
